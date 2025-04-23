9a2d5eb0a2b44a45e21e29d6f33ac78eb152d50d
prostatus <mel.sirvania@gmail.com>
2025-04-23 22:41:09 +0300

    Initial commit, BinReader and View added

---
+++ Melnik_tomogram_visualiser/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Melnik_tomogram_visualiser
+{
+    public partial class Form1 : Form
+    {
+
+        Bin bin;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bin = new Bin();
+        }
+    }
+}
---
+++ Melnik_tomogram_visualiser/View.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melnik_tomogram_visualiser
+{
+    internal class View
+    {
+
+        public View() { }
+
+        public void SetupView(int width, int height)
+        {
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+            GL.Viewport(0, 0, width, height);
+        }
+    }
+}
